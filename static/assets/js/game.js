1a31426bafd03ae921d5b0b4cac5afd5d16c7b9e
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2023-01-19 15:33:10 -0700

    add search system and local game system

--- static/assets/js/game.js
+++ static/assets/js/game.js
@@ -1,9 +1,27 @@
-/*fetch('/assets/JSON/gs.json')
+var loaded = false;
+
+fetch('/assets/JSON/gs.json')
     .then((res) => res.json())
     .then((games) => {
-    });*/
+        for (let i = 0; i < games.length; i++) {
+            const game = games[i];
 
-fetch('https://api.gh.retronetwork.ml/games')
+            var gameEl = document.createElement('div');
+            gameEl.classList = 'game';
+            gameEl.title = game.name;
+            gameEl.innerHTML = `<img src="${game.thumbnail}"/><p>${game.name}</p>`;
+            document.querySelector('.games').appendChild(gameEl);
+            gameEl.addEventListener('click', (e) => {
+                
+            });
+        }
+
+        loaded = true;
+    }).catch((e) => {
+        alert(e);
+    })
+
+fetch('https://gamehubapi.onrender.com/games')
     .then((res) => res.json())
     .then((games) => {
         for (let i = 0; i < games.length; i++) {
@@ -18,6 +36,95 @@ fetch('https://api.gh.retronetwork.ml/games')
                 alert(game.id);
             });
         }
+
+        loaded = true;
     }).catch((e) => {
         console.log(e);
     })
+
+const searchBar = document.querySelector('#search');
+searchBar.addEventListener('input', (e) => {
+    if (loaded) {
+        if (searchBar.value) {
+            const searchQuery = searchBar.value.toLowerCase();
+            let searchResults = 0;
+            let gamesSearched = 0;
+
+            for (let i = 0; i < document.querySelectorAll('.game').length; i++) {
+                const game = document.querySelectorAll('.game')[i];
+
+                if (!game.title.toLowerCase().includes(searchQuery)) {
+                    game.classList.add('is-hidden');
+                } else {
+                    game.classList.remove('is-hidden');
+                    searchResults += 1;
+                }
+
+                gamesSearched += 1;
+            }
+
+            var searchInterval = setInterval(() => {
+                if (gamesSearched === document.querySelectorAll('.game').length) {
+                    clearInterval(searchInterval)
+
+                    if (searchResults === 0) {
+                        document.querySelector('.no_results').classList.remove('is-hidden');
+                        document.querySelector('#searchQuery').textContent = searchBar.value;
+                    } else {
+                        document.querySelector('.no_results').classList.add('is-hidden');
+                    }
+                }
+            }, 10);
+        } else {
+            for (let i = 0; i < document.querySelectorAll('.game').length; i++) {
+                const game = document.querySelectorAll('.game')[i];
+                game.classList.remove('is-hidden');
+            }
+
+            document.querySelector('.no_results').classList.add('is-hidden');
+        }
+    }
+})
+
+var searchInterval = setInterval(() => {
+    if (loaded) {
+        if (searchBar.value) {
+            const searchQuery = searchBar.value.toLowerCase();
+            let searchResults = 0;
+            let gamesSearched = 0;
+
+            for (let i = 0; i < document.querySelectorAll('.game').length; i++) {
+                const game = document.querySelectorAll('.game')[i];
+
+                if (!game.title.toLowerCase().includes(searchQuery)) {
+                    game.classList.add('is-hidden');
+                } else {
+                    game.classList.remove('is-hidden');
+                    searchResults += 1;
+                }
+
+                gamesSearched += 1;
+            }
+
+            var searchInterval = setInterval(() => {
+                if (gamesSearched === document.querySelectorAll('.game').length) {
+                    clearInterval(searchInterval)
+
+                    if (searchResults === 0) {
+                        document.querySelector('.no_results').classList.remove('is-hidden');
+                        document.querySelector('#searchQuery').textContent = searchBar.value;
+                    } else {
+                        document.querySelector('.no_results').classList.add('is-hidden');
+                    }
+                }
+            }, 10);
+        } else {
+            for (let i = 0; i < document.querySelectorAll('.game').length; i++) {
+                const game = document.querySelectorAll('.game')[i];
+                game.classList.remove('is-hidden');
+            }
+
+            document.querySelector('.no_results').classList.add('is-hidden');
+        }
+    }
+}, 10)
