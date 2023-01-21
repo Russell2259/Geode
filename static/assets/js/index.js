c6b37f9810a5566b69d836bc0f22d33dbdf251ff
Russell <84256826+Russell2259@users.noreply.github.com>
2023-01-21 04:28:20 -0700

    added search API

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -17,3 +17,25 @@ form.addEventListener('submit', async (event) => {
   const url = search(address.value, 'https://www.google.com/search?q=%s');
   location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
+
+address.addEventListener('input', (e) => {
+  if (!address.isURL(address.value) && address.value) {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-BingApis-SDK': 'true',
+        'X-RapidAPI-Key': '584c8a4891msh0fdd1285155e729p1a16c2jsnac8aa6f24523',
+        'X-RapidAPI-Host': 'bing-web-search1.p.rapidapi.com'
+      }
+    };
+
+    fetch(`https://bing-web-search1.p.rapidapi.com/search?q=${address.value}&mkt=en-us&setLang=en-us&count=5&safeSearch=Strict&textFormat=Raw&freshness=Week`, options)
+      .then(bare => bare.json())
+      .then(res => {
+        console.log(response)
+      })
+      .catch(err => {
+        throw new Error(err)
+      });
+  }
+})
